5c2fa6cddd31f3fd15e0b85d09cf1abc76e9dbd9
EmanNurein <eamw96:gmail.com>
2022-06-24 01:17:43 +0200

    First Commit

---
+++ apiDocument.md
@@ -0,0 +1,242 @@
+# Page 1
+
+# List of city
+
+#https://localhost:9700/location
+
+#https://pharm-project.herokuapp.com/location
+
+# List of pharmacy
+
+#http://localhost:9700/pharmacy
+
+#https://pharm-project.herokuapp.com/pharmacy
+
+# List of pharmacy wrt to city
+
+#http://localhost:9700/pharmacy/?state_id=1
+
+#https://pharm-project.herokuapp.com/pharmacy/?state_id=1
+
+# List of productTypes
+
+#http://localhost:9700/producttype
+
+#https://pharm-project.herokuapp.com/producttype
+
+# Page 2
+
+# List of pharmacy on basis of producttype
+
+#http://localhost:9700/pharmacy/?producttype_id=2
+
+#https://pharm-project.herokuapp.com/pharmacy/?producttype_id=2
+
+# pharmacy on basis of product selected
+
+#(POST) http://localhost:9700/product
+
+        (Body) [6]
+        [
+            {
+                "\_id": "626688d2c7d11798ac3a956b",
+                "pharmacy_id": 2,
+                "pharmacy_name": "Almek Nimer Pharmacy",
+                "location_id": 2,
+                "state_id": 2,
+                "product_id": 6,
+                "address": "Amarat - Street No 15",
+                "pharmacy_image": "https://i.ibb.co/wJ1tPz2/service-cust.jpg",
+                "contact_number": "0912312312",
+                "productTypes": [
+                {
+                "producttype_id": 2,
+                "producttype_name": "Liquid"
+                },
+                {
+                "producttype_id": 4,
+                "producttype_name": "Capsule"
+                }
+                ],
+                "image_gallery": [
+                "https://i.ibb.co/7vJMw90/liquid-image.jpg",
+                "https://i.ibb.co/bXQrT8Y/capsule2.jpg"
+                ]
+            }
+        ]
+
+# Filter on basis of producttype
+
+#http://localhost:9700/filters/1?productId=1
+
+#https://pharm-project.herokuapp.com/filters/1?productId=1
+
+# Filter on basis of cost
+
+#http://localhost:9700/filters/2?lcost=1000&hcost=4000
+
+#https://pharm-project.herokuapp.com/filters/2?lcost=1000&hcost=4000
+
+# Sort (Low & High)
+
+#http://localhost:9700/filters/3?productId=1&sort=-1
+
+#https://pharm-project.herokuapp.com/filters/3?productId=1&sort=-1
+
+# page 3
+
+# Details of Pharmacy
+
+#http://localhost:9700/details/62933c2023f40402bd446241
+
+#https://pharm-project.herokuapp.com/details/62933c2023f40402bd446241
+
+# Products on basis of pharmacy
+
+#http://localhost:9700/products/?pharmId=1
+
+#https://pharm-project.herokuapp.com/products/?pharmId=1
+
+# Page 4
+
+# Products detail of item selected
+
+#(POST) http://localhost:9700/productItem
+
+#(Body) [1,2,3]
+
+#(POST) https://pharm-project.herokuapp.com/productItem
+
+#(Body) [1,3,6]
+
+# Page 5
+
+# Place Order
+
+#(POST) http://localhost:9700/placeOrder
+
+#<Body)
+
+            {
+                "name": "Nada",
+                "email":"nada@gmail.com",
+                "address": "Almuhandseen Block No.30",
+                "phone": "092224578",
+                "cost": 2300,
+                "productItem":[3,6],
+                "status":"pending"
+            }
+
+#(POST) https://pharm-project.herokuapp.com/placeOrder
+
+#(Body)
+
+        {
+            "name": "Salma",
+            "email":"salma123@gmail.com",
+            "address": "Khartoum Bahri",
+            "phone": "0123123456",
+            "cost": 4000,
+            "productItem":[5,6],
+            "status":"pending"
+        }
+
+# See all place order
+
+#http://localhost:9700/viewOrder
+
+#https://pharm-project.herokuapp.com/viewOrder
+
+# Get order on basis of emailId
+
+#http://localhost:9700/viewOrder?email=eman@gmail.com
+
+#https://pharm-project.herokuapp.com/viewOrder/?eman@gmail.com
+
+# Update order
+
+#(PUT) http://localhost:9700/updateOrder/62575a0400178fa239ef724c
+
+#(Body)
+
+        {
+            "status":"In Transit",
+            "bankName":"Axis bank"
+        }
+
+#(PUT) https://pharm-project.herokuapp.com/updateOrder/62575a0400178fa239ef724c
+
+#(Body)
+
+        {
+            "status":"In Transit",
+            "bankName":"BlueVine"
+        }
+
+# login Api
+
+# GetAllUser
+
+#(GET) http://localhost:5000/api/auth/users
+
+#(GET) https://Pharm-loginapp.herokuapp.com/api/auth/users
+
+# Register
+
+#(POST) http://localhost:5000/api/auth/register
+
+#(Body)
+
+            {
+                name":"Ahmed Ali",
+                "email":"ahmed@hotmail.com",
+                "password":"ahmedali",
+                "phone":"0914542545",
+                "role":"user"
+            }
+
+#(post) https://Pharm-loginapp.herokuapp.com/api/auth/register
+
+#(Body)
+
+            {
+                name":"Ahmed Ali",
+                "email":"ahmed@hotmail.com",
+                "password":"ahmedali",
+                "phone":"0914542545",
+                "role":"user"
+            }
+
+# Login
+
+#(POST) http://localhost:5000/api/auth/login
+
+#(body)
+
+            {
+                "email":"ahmed@hotmail.com",
+                "password":"ahmedali"
+            }
+
+#(Response)=> {auth:true,token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'}
+
+#(POST) https://Pharm-loginapp.herokuapp.com/api/auth/login
+
+#(Body)
+
+            {
+                "email":"ahmed@hotmail.com",
+                "password":"ahmedali"
+            }
+
+#(Response) => {"auth": true, "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"}
+
+# UserInfo
+
+#(GET) http://localhost:5000/api/auth/userinfo
+
+#(Header) {'x-access-token':'token value from login'}
+
+#(GET) https://Pharm-loginapp.herokuapp.com/api/auth/userinfo
+
+#(Header) {'x-access-token':'token value from login'}
---
+++ app.js
@@ -0,0 +1,224 @@
+let express = require('express');
+let app = express();
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const dotenv = require('dotenv');
+dotenv.config()
+let port = process.env.PORT || 8230;
+// const mongoUrl = process.env.mongoUrl;
+// const mongoUrl = "mongodb://localhost:27017";
+// const mongoUrl = "process.env.mongoUrl";
+// const mongoUrl = "mongodb+srv://eman:QT8LoVUWDt0uSVFu@cluster0.bvz0d.mongodb.net/emaapharmacy?retryWrites=true&w=majority";
+const mongoUrl = "mongodb+srv://eman:MscV9x0sGQfgsrIJ@cluster0.bvz0d.mongodb.net/emaapharmacy?retryWrites=true&w=majority";
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+
+
+
+    // middleware
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json())
+    app.use(cors())
+
+    
+    app.get('/',(req,res) => {
+
+        res.send("Welcome to Express")
+    })
+
+
+    //location 
+        app.get('/location',(req,res) => {
+            //if(req.query.token === token) {
+            db.collection('location').find().toArray((err,result) => {
+                if (err) throw err ;
+                res.send(result)
+            })
+           
+        })
+
+    //Pharmacy 
+    
+        app.get('/pharmacy/',(req,res) => {
+        // let id = req.params.id;
+        // let id  = req.query.id
+        // console.log(">>>id",id)
+        let query = {};
+        let stateId = Number(req.query.stateId)
+        let productId = Number(req.query.producttype_id)
+        
+        if(stateId){
+            query = {state_id:stateId}
+        }
+        else if(productId){
+            query = {'productTypes.producttype_id':productId}
+        }
+
+        db.collection('pharmacy').find(query).toArray((err,result) => {
+            if(err) throw err;
+            res.send(result)
+        })
+    })
+    
+
+    app.get('/filters/:productId',(req,res) => {
+        let sort = {cost:1}
+        let productId = Number(req.params.productId)
+        let lcost = Number(req.query.lcost)
+        let hcost = Number(req.query.hcost)
+    
+        let query = {}
+        if(req.query.sort){
+            sort={cost:Number(req.query.sort)}
+
+        }else if(lcost && hcost){
+            query = {
+                "productTypes.producttype_id":productId,
+                $and:[{cost:{$gt:lcost,$lt:hcost}}]
+            }
+        }
+        else{
+            query = {
+                "productTypes.producttype_id":productId
+            }
+        }
+    
+        db.collection('pharmacy').find(query).sort(sort).toArray((err,result) => {
+            if(err) throw err;
+            res.send(result)
+        })
+    })
+
+    //productTypes 
+    app.get('/producttype',(req,res) => {
+        //if(req.query.token === token) {
+        db.collection('productTypes').find().toArray((err,result) => {
+            if (err) throw err ;
+            res.send(result)
+        })
+    })
+
+
+     //PharmacyDetails
+        app.get('/details',(req,res) => {
+            //let pharmId = Number(req.params.id);
+            let pharmId = mongo.ObjectId(req.params.id)
+            db.collection('pharmacy').find({_id:pharmId}).toArray((err,result) => {
+                if(err) throw err;
+                res.send(result)
+        })
+     })
+
+     //products
+        app.get('/products',(req,res) => {
+            let query = {}
+            let pharmId = Number(req.query.pharmId)
+            // let producttypeId = Number(req.query.producttype_id)
+            if(pharmId){
+                query = {pharmacy_id:pharmId}
+            }
+            // else if(producttypeId){
+            //     query = {producttype_id:producttypeId}
+            // }
+            db.collection('products').find(query).toArray((err,result) => {
+                if(err) throw err;
+                res.send(result)
+        })
+    })
+        
+
+    //pharmacy on basis of product selected
+        app.post('/product',(req,res) => {
+            console.log(req.body);
+            if(Array.isArray(req.body)){
+                db.collection('pharmdetails').find({product_id:{$in:req.body}}).toArray((err,result) => {
+                    if(err) throw err;
+                    res.send(result)
+                })
+            }else{
+                res.send('Invalid Input')
+            }
+        })
+        
+    // product on basis of item selected
+        app.post('/productItem',(req,res) => {
+            console.log(req.body);
+            if(Array.isArray(req.body)){
+                db.collection('products').find({product_id:{$in:req.body}}).toArray((err,result) => {
+                    if(err) throw err;
+                    res.send(result)
+                })
+            }else{
+                res.send('Invalid Input')
+            }
+        })
+
+    // place Order
+        app.post('/placeOrder',(req,res) => {
+            db.collection('orders').insert(req.body,(err,result) => {
+                if(err) throw err;
+                res.send('Order Placed')
+            })
+        })
+
+
+    // View Order
+        app.get('/viewOrder',(req,res) => {
+            let email = req.query.email;
+            let query = {};
+            if(email){
+                query = {"email":email}
+            }
+            db.collection('orders').find(query).toArray((err,result) => {
+                if(err) throw err;
+                res.send(result)
+            })
+        })
+
+    // delete order
+        app.delete('/deleteOrders',(req,res)=>{
+            db.collection('orders').remove({},(err,result) => {
+                res.send('order deleted')
+            })
+        })
+
+
+    //update orders
+        app.put('/updateOrder/:id',(req,res) => {
+            let oId = Number(req.params.id)
+            // let oId = mongo.ObjectId(req.params.id);
+            db.collection('orders').updateOne(
+                // {_id:oId},
+                {id:oId},
+                {$set:{
+                    "email":req.body.email,
+                    "address":req.body.address,
+                    "status":req.body.status,
+                    "bank_name":req.body.bank_name,
+                    "date":req.body.date,
+                    
+                }},(err,result) => {
+                    if(err) throw err
+                    res.send(`Status Updated to ${req.body.status}`)
+                }
+            )
+        })
+          
+
+        
+    // connect to database
+    MongoClient.connect(mongoUrl, (err,client) => {
+
+        if (err) console.log('Error while connecting');
+        db = client.db('emaapharmacy');
+
+        app.listen(port, () => {
+
+        console.log(`Sever is running on port  ${port}`)
+
+        })
+    })
+
+
+   
